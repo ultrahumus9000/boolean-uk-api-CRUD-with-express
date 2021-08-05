ed63573cf1ae64e8f187d2753d87335211bae802
Linlin Li <paris1900lll@gmail.com>
2021-08-05 23:34:34 +0100

    half

--- src/pet/model.js
+++ src/pet/model.js
@@ -1,5 +1,25 @@
 const db = require("../database/database");
 
-function Pet() {}
+function Pet() {
+  function createTable() {}
+
+  function createOnePet() {}
+
+  function findAllPets() {}
+
+  function findOnePet() {}
+
+  function deleteOnePet() {}
+
+  function updateOnePet() {}
+
+  return {
+    createOnePet,
+    findAllPets,
+    findOnePet,
+    deleteOnePet,
+    updateOnePet,
+  };
+}
 
 module.exports = Pet;
